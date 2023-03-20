5c2c0ae9f172c6703d368e6960b047e2875de6a8
Chrigor <Chrigorgamer@gmail.com>
2023-03-20 18:20:09 -0300

    Incremento de funções

--- src/Livaria/Livraria.java
+++ src/Livaria/Livraria.java
@@ -1,30 +1,38 @@
 package Livaria;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import Controle.CCliente;
 import Controle.CEditora;
 import Controle.CLivro;
 import Controle.CVendaLivro;
+import Model.Cliente;
+import util.Validadores;
 
-public class Livraria {
+public class Livraria { // Incio CLASS
     public static CCliente CadCliente = new CCliente();
     public static CEditora CadEditora = new CEditora();
     public static CLivro CadLivro = new CLivro();
     public static CVendaLivro CadVendaLivro = new CVendaLivro();
+    public static Scanner ler = new Scanner(System.in);
 
-    public static int LeiaNumINT() {
-        try {
-            Scanner leia = new Scanner(System.in);
-            return leia.nextInt();
-        } catch (Exception e) {
-            System.out.println("Tente novamente!");
-            LeiaNumINT();
-        }
-        return 0;
-    }
+    public static int LeiaNumINT() {// Inicio LEIA
+        int num = 99;
+        boolean leu = false;
+        Scanner lerNum = new Scanner(System.in);
+        while(!leu){
+            try{
+                num = lerNum.nextInt();
+                leu = true;
+            } catch (InputMismatchException e){
+                System.out.println("Entrada inválida, tente novamente!");
+                ler.nextLine():
+            }
+        } return num;
+    }// Fim LEIA
 
-    public static void MenuP() {
+    public static void MenuP() {// Inicio MENU
         System.out.println();
         System.out.println();
         System.out.println("|-------------------|");
@@ -38,9 +46,9 @@ public class Livraria {
         System.out.println("|-------------------|");
         System.out.print(" Escolha uma opção: ");
 
-    }
+    }// Fim MENU
 
-    public static void SubMenu(int op) {
+    public static void SubMenu(int op) {// Inicio SUBMENU
         String tpCad = null;
         switch (op) {
             case 1:
@@ -65,9 +73,76 @@ public class Livraria {
         System.out.println("| 0 - Voltar                   |");
         System.out.println("|------------------------------|");
         System.out.print(" Escolha uma opção: ");
-    }
+    }// Fim SUBMENU
 
-    public static void main(String[] args) {
+    public static void cadastrarCliente() {// Inicio CADCLIENTE
+        int idCliente;
+        String nomeCliente;
+        String cpf;
+        String cnpj = null;
+        String endereco;
+        String telefone;
+
+        System.out.println("|Cadastro de Cliente|");
+        System.out.print("Informe o CPF:");
+        boolean cpfis;
+        int opCPF;
+        do {// Inicio DO
+            cpf = ler.nextLine();
+            cpfis = Validadores.isCPF(cpf);
+            if (!cpfis) {
+                System.out.println("CPF inválido" + "\nDeseja tentar novamente ? 1 - Sim | 2 - Não");
+                opCPF = LeiaNumINT();
+
+                if (opCPF == 1) {
+                    System.out.println("Informe o CPF:");
+                } else if (opCPF == 2) {
+                    System.out.println("Cadastro cancelado pelo usuário");
+                    break;
+                }
+            }
+        } while (!Validadores.isCPF(cpf));
+        if (CadCliente.getClienteCPF(cpf) != null) {
+            System.out.println("Cliente já cadastrado");
+        } else {
+            System.out.println("Informe o nome: ");
+            nomeCliente = ler.nextLine();
+            System.out.println("Informe o telefone: ");
+            telefone = ler.nextLine();
+            System.out.println("Informe o endereço: ");
+            endereco = ler.nextLine();
+            idCliente = CadCliente.geraID();
+            Cliente cli = new Cliente(idCliente, nomeCliente, cpf, cnpj, endereco, telefone);
+            CadCliente.addCliente(cli);
+            System.out.println("Cliente cadastro com sucesso!");
+
+        } // Fim DO
+    }// Fim CADCLIENTE
+
+    public static void deletarCliente() {// Inicio DELETAR
+        System.out.println("|Deletar Cliente|");
+        System.out.print("Informe o CPF: ");
+        String cpf = ler.next();
+        if (Validadores.isCPF(cpf)) {
+            Cliente cli = CadCliente.getClienteCPF(cpf);
+            if (cli != null) {
+                System.out.println("Cliente deletado com sucesso!.");
+            } else {
+                System.out.println("Cliente não consta na base de dados.");
+            }
+        } else
+            System.out.println("CPF Inválido!.");
+    }// Fim DELETAR
+
+    public static void listarCliente() {// Inicio LISTAR
+        for (Cliente cli : CadCliente.GetClientes()) {
+            System.out.println("\nCPF: " + cli.getCpf());
+            System.out.println("Nome: " + cli.getNomeCliente());
+            System.out.println("Telefone: " + cli.getTelefone());
+        }
+    }// Fim LISTAR
+
+    public static void main(String[] args) {// Inicio VOID
         int opM;
         do {
             MenuP();
@@ -114,5 +189,5 @@ public class Livraria {
                     break;
             }
         } while (opM != 0);// Sistema
-    }
-}
+    }// Fim VOID
+}// Fim CLASS
